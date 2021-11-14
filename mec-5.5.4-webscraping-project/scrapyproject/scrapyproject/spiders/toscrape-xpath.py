6740d5f5b65e82bbf17b7bc68e08fecea3b3c584
Aaquila Mariajohn <Aaquilaa.arzela@gmail.com>
2021-11-13 17:44:51 -0800

    with xpath results

--- mec-5.5.4-webscraping-project/scrapyproject/scrapyproject/spiders/toscrape-xpath.py
+++ mec-5.5.4-webscraping-project/scrapyproject/scrapyproject/spiders/toscrape-xpath.py
@@ -2,20 +2,20 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+    name = "quotes2"
 
     start_urls = [
             'http://quotes.toscrape.com/page/1/',
         ]
 
     def parse(self, response):
-        for quote in response.xpath('//div/div/div/div/*'): #'//*[contains(name(),quote)]'):
+        for quote in response.xpath("//div[contains(@class, 'quote')]"):
             yield {
-              'text': quote.css('span.text::text').get(),
-              'author': quote.css('small.author::text').get(),
-              'tags': quote.css('div.tags a.tag::text').getall(),
+                'text': quote.xpath("span[contains(@class, 'text')]/text()").get(),
+                'author': quote.xpath("span/small[contains(@class, 'author')]/text()").get(),
+                'tags': quote.xpath("div[contains(@class,'tags')]/a[contains(@class, 'tag')]/text()").getall()
                 }
 
-        next_page = response.css('li.next a::attr(href)').get()
+        next_page = quote.xpath("//li[@class='next']/a").attrib['href']
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
